2472f740c47ab030ecad61f1405db22aea79d0aa
amnigam <king.amit95@gmail.com>
2024-10-31 03:00:22 +0530

    Func Delegates added.

---
+++ Delegates/Delegates/Delegates/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    internal class Discount
+    {
+        // This class defines a method that can be used to apply discounts to a List of totals.
+        // It takes a list of totals and a Function Delegate that has an input of decimal and output of decimal. 
+        public void ApplyDiscounts (List<decimal> orderTotals, Func<decimal, decimal>discountStrategy)
+        {
+            Console.WriteLine("[+] Discounted Totals");
+            Console.WriteLine("-------------------------------");
+
+            foreach (var total in orderTotals)
+            {
+                decimal discountedTotal = discountStrategy(total);  // Discounted value for the element in Total's List is computed by passing it to func delegate
+                Console.WriteLine($"Original Price: {total},    Discounted Price: {discountedTotal}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Delegates/Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic; 
 
 namespace Delegates
 {
@@ -23,8 +24,24 @@ namespace Delegates
             // Next, we remove the method by simply using -= 
             ld -= lg.LogToConsole;      // We are saying to log only to file now. 
 
-            ld("Well, file is the way for me to log stuff!."); 
+            ld("Well, file is the way for me to log stuff!.");
 
+            // Here we look at the Func Delegate referenced in our Discount class.
+
+            // We first define a Discount strategy.
+            Func<decimal, decimal> standardDiscount = total => total * 0.9m;
+            Func<decimal, decimal> premiumDiscount = total => total * 0.8m;
+            Func<decimal, decimal> bulkDiscount = total => total > 1000 ? total * 0.7m : total * 0.9m;
+
+            // Create a List of totals to apply discounts to.
+            List<decimal> orderTotals = new List<decimal> { 400, 1200, 2000, 700, 9000, 900, 50 }; 
+
+            // Create an object of Discount type - this gives the applyDiscount method to the object.
+            Discount ds = new Discount();
+
+            ds.ApplyDiscounts(orderTotals, standardDiscount); 
+            ds.ApplyDiscounts(orderTotals, premiumDiscount);
+            ds.ApplyDiscounts(orderTotals, bulkDiscount);
         }
     }
 }
